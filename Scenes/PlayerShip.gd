f2471a7e20b088a69920908a8bc56016d7d9986e
Reza Ghassemi <rghassem@gmail.com>
2024-08-17 03:30:01 -0700

    initial exterior scenes with some ships, asteroids, and tank controls

---
+++ Scenes/PlayerShip.gd
@@ -0,0 +1,34 @@
+extends RigidBody2D
+
+@export var thrusts = [0, 10, 50, 100, 200]
+
+var currentThrustIdx = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	angular_damp = 1
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(_delta):
+
+	if (Input.is_action_just_pressed("space")):
+		currentThrustIdx = (currentThrustIdx + 1) % len(thrusts)
+
+	if (Input.is_action_pressed("left")):
+		apply_torque_impulse(100)
+
+	if (Input.is_action_pressed("right")):
+		apply_torque_impulse(-100)
+
+	# Rotational damping
+
+	# Get the orientation vector of the rigitbody 
+	var orientation = -global_transform.y.normalized()
+	var force = orientation * thrusts[currentThrustIdx]
+	apply_central_force(force)
+	pass
