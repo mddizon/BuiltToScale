410a9396ae03f668e250776a4e6a616b72d13b13
Mark Dizon <markdizon07@gmail.com>
2024-08-17 13:32:57 -0700

    type

--- Game.gd
+++ Game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var interior = preload("res://Scenes/ship_interior.tscn").instantiate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,6 +9,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	print('process' + str(_delta))
+	if (Input.is_action_just_pressed("zoom-out")):
+		#render interior on top of the current scene
+		get_tree().root.add_child(interior)
+		z_index = -5
+		interior.z_index = 0
+	elif (Input.is_action_just_pressed("zoom-in")):
+		interior.z_index = -5
+		z_index = 0
 	pass
 
 
