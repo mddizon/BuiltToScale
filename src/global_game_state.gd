b28bb2da1effd3ed3141dc449807fc41eaa59345
Mark Dizon <markdizon07@gmail.com>
2024-08-18 14:03:10 -0700

    add health bar

--- src/global_game_state.gd
+++ src/global_game_state.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 var invert_zoom = false
 var current_zoom_inputs = 0
+var player_health = 100
+
+func reset_game():
+	player_health = 100
--- src/user_interface.gd
+++ src/user_interface.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SignalBus.player_damage_taken.connect(damage_taken)
+	$Healthbar.init_health(GlobalGameState.player_health)
 	#var shader_material = ShaderMaterial.new()
 	#shader_material.set_shader_param("screen_texture", get_viewport().get_texture())
 
@@ -15,5 +16,7 @@ func _process(_delta):
 	pass
 
 func damage_taken(_damage: int):
-	pass
+	GlobalGameState.player_health -= _damage	
+	$Healthbar.health = GlobalGameState.player_health
+	
 	#$GlitchScreen.material = glitch_material
