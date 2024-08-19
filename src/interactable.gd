d1dc0698e347ac3d9f954a18c09bf950c9cdb345
Mark Dizon <markdizon07@gmail.com>
2024-08-18 20:11:09 -0700

    Add system tooltip

--- src/interactable.gd
+++ src/interactable.gd
@@ -2,27 +2,24 @@ extends Sprite2D
 
 @export var action_name: String
 @export var threshhold_distance: float = 130
+@export_multiline var description: String = "Add description here"
 
 var player: CharacterBody2D
-@onready var label = $Label
+var player_near = false
+@onready var text_container = $CanvasLayer/ColorRect3
+@onready var text_description = $CanvasLayer/ColorRect3/RichTextLabel
 
 signal interacted(action_name: String)
 
 func _ready():
 	add_to_group("interactable")
 	player = get_parent().get_node("InteriorCharacter")
-	label.text = action_name
-	label.visible = false
+	text_container.visible = false
+	text_description.text = description
 	assert(player, "Player not found")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var player_near = is_player_near();
-	label.visible = player_near
-	if player_near:
-		modulate = Color(1, 1, 1, .5)
-	else:
-		modulate = Color(1, 1, 1, 1)
 	if player_near and Input.is_action_just_pressed("action"):
 		emit_signal("interacted", action_name)
 
@@ -32,3 +29,15 @@ func is_player_near():
 	var dist = pos1.distance_to(pos2)
 	# print(dist)
 	return dist < threshhold_distance
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group('player'):
+		player_near = true
+		text_container.visible = true
+		modulate = Color(1, 1, 1, .5)
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group('player'):
+		player_near = false
+		text_container.visible = false
+		modulate = Color(1, 1, 1, 1)
